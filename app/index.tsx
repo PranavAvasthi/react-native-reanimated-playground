cbe757f73828876d9bcf8180b35417a6c6282446
PranavAvasthi <pranavavasthi44@gmail.com>
2026-02-05 00:04:52 +0530

    chore: nativewind setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Text, View } from 'react-native';
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    return (
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
+    );
+};
 
 export default App
